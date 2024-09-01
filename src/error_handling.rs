0d096a3be1b00c67239e3154d484569806076f9a
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-09-01 13:02:26 -0300

    Improved Error Handling using Display() via Implement Thanks to Wgaffa!

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -1,4 +1,5 @@
 // --- error_handling
+use std::fmt;
 pub enum AppErrors {
     IAmAlright,
     GenericError,
@@ -7,21 +8,24 @@ pub enum AppErrors {
     ValidChapterList,
     ValidSceneList,
     ValidPartIndex,
+    ReadableFile,
     PlainTextString,
 }
 
-pub fn getExpected(inc_err: AppErrors) -> String {
-    let user_friendly = match inc_err {
-        AppErrors::IAmAlright => "OK01 - Not An Error.",
-        AppErrors::VaildPath => "ER01 - Expected valid Content Path",
-        AppErrors::ValidPartList => "ER02 - Expected valid list of Book Parts",
-        AppErrors::PlainTextString => "ER03 - Expected title to be Plain Text",
-        AppErrors::ValidChapterList => "ER04 - Expected valid list of Chapters",
-        AppErrors::ValidSceneList => "ER05 - Expected valid list of Scenes",
-        AppErrors::ValidPartIndex => "ER06 - That Part of the Book cannot be found",
-        _ => "ER13 - Generic Error - Write Better Code",
-    };
-
-    user_friendly.into()
+impl fmt::Display for AppErrors {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            AppErrors::IAmAlright => write!(f, "OK01 - Not An Error."),
+            AppErrors::VaildPath => write!(f, "ER01 - Expected valid Content Path"),
+            AppErrors::ValidPartList => write!(f, "ER02 - Expected valid list of Book Parts"),
+            AppErrors::PlainTextString => write!(f, "ER03 - Expected title to be Plain Text"),
+            AppErrors::ValidChapterList => write!(f, "ER04 - Expected valid list of Chapters"),
+            AppErrors::ValidSceneList => write!(f, "ER05 - Expected valid list of Scenes"),
+            AppErrors::ValidPartIndex => write!(f, "ER06 - That Part of the Book cannot be found"),
+            AppErrors::ReadableFile => write!(f, "ER07 - Should have been able to read the file"),
+            _ => write!(f, "ER13 - Generic Error - Write Better Code"),
+        }
+    }
 }
+
 // --- error_handling
