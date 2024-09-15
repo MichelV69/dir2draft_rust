2c92684d40b7348f816cadf8b6d3a0e581254fef
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-09-15 20:15:26 -0300

    start prepping for main code loop

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@
 extern crate walkdir;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
 use walkdir::WalkDir;
 
 // ---- modules
@@ -30,42 +31,24 @@ fn main() {
     let content_path = "./content";
     let output_file = "my_book_title";
 
-    // ---- main code
-    //   let mut project = Book::new();
-    //   let mut write_ptr =
-    //       File::create(format!("{}.md", output_file))
-    //       .expect(&getErr(appErrors::noVaildPath));
+    todo!("app get args for content path, ");
+    todo!("app get args for output file name");
+    todo!("app get args for book title.to_display");
 
-    //    if path_elemets.len() == 3 {
-    //        let found_part = path_elemets[0];
-    //        let found_chapter = path_elemets[1];
+    let mut my_app = AppCfg::new();
+    my_app.content_path = "./content".into();
+    my_app.output_file = "my_book_title".into();
+    let path_elm = AppCfg::get_path_elements(&my_app.content_path.clone());
 
-    //          if project.is_a_new_part(found_part) {
-    //              let new_part = Part::new();
-    //              new_part.title.sort_by = found_part.to_string();
-    // -- set the reader-friendly title for the part
-    //              project.part_list.push(new_part);
+    let mut this_book = Book::new();
+    for dir_entry in &path_elm {
+        this_book.add_content(&my_app, dir_entry);
+    }
 
-    //          }
+    let path_string = &format!("{}/../{}.md", &my_app.content_path, &my_app.output_file);
+    let work_path = Path::new(path_string);
+    let mut work_file = File::create(work_path).expect(&format!("{}", AppErrors::VaildPath));
 
-    //          let mut current_part = project.for_part(found_part);
-    //          if current_part.is_a_new_chapter(found_chapter) {
-    //              let new_chapter = Chapter::new();
-    //              new_chapter.title.sort_by = found_chapter;
-    // -- set the reader-friendly title for the chapter
-    //              current_part.part_list.push(new_chapter);
-    //          }
-    //    }
-
-    // --- writitng to disk
-    // --- --- sort part List
-    // --- --- loop part List
-    // --- --- --- sort chapter List
-    // --- --- --- loop chapter List
-    // --- --- --- --- sort scene List
-    // --- --- --- --- loop scene List
-    // --- --- --- --- first_time print_part_title
-    // --- --- --- --- first_time print_chapter_title
-    // --- --- --- --- print scene_title
-    // --- --- --- --- print scene_text
+    Book::write_toc(this_book.clone(), &mut work_file);
+    Book::write_content(this_book, &mut work_file);
 }
